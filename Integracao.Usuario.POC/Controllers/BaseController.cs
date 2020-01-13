3a661e09b439591e298882dd49583ced4dc33d73
olavomoncaio <boltxs@gmail.com>
2020-01-12 21:21:04 -0300

    Remoção de bibliotecas no BaseController que não estavam sendo usadas

--- Integracao.Usuario.POC/Controllers/BaseController.cs
+++ Integracao.Usuario.POC/Controllers/BaseController.cs
@@ -1,9 +1,4 @@
 ﻿using Integracao.Usuario.POC.Interfaces;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Threading.Tasks;
 
 namespace Integracao.Usuario.POC.Controllers
 {
